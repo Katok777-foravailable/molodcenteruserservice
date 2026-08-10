98572de636ca414d4dde9d53bad056934daa60ff
Katok777-foravailable <svatoslav22882288@gmail.com>
2026-08-10 12:24:55 +0300

    Initial commit

---
+++ src/main/java/com/katok/molodcenteruserservice/MolodcenteruserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.katok.molodcenteruserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolodcenteruserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolodcenteruserserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/katok/molodcenteruserservice/MolodcenteruserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katok.molodcenteruserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MolodcenteruserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
